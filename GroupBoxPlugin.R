d09c200e427a219a680249b48d90ab42842cf14e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-06-23 08:45:10 -0400

    Initial

---
+++ GroupBoxPlugin.R
@@ -0,0 +1,49 @@
+library(ggpubr)
+library(gplots)
+library(viridis)
+library(hrbrthemes)
+library(ggsci)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
+input <- function(inputfile) {
+ print("HI")
+  pfix = prefix()
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+  print(parameters)
+   # Need to get the three files
+   assay <<- read.table(paste(pfix, parameters["abundance", 2], sep="/"), header = TRUE, sep=',')
+   #x <<- textConnection(paste(pfix, parameters["taxa", 2], sep="/"))
+   #x <<- readLines(con="pipelines/GulfWar/taxa.diet.txt")#textConnection(con="pipelines/GulfWar/taxa.diet.txt"))
+   #print(x)
+   species_combined_list <<- readLines(paste(pfix, parameters["taxa", 2], sep="/"))
+   print("SP COMB")
+   print(species_combined_list)
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+
+	# Distinct colors:
+library(RColorBrewer)
+colourCount = 4
+getPalette = colorRampPalette(brewer.pal(4, "Paired"))
+
+for(species in species_combined_list) {
+#  print(species)
+#
+  but_plot <- ggboxplot(assay, x="Group" , y = species,
+                        color = "black",  fill="Group") +
+    theme(axis.title.x = element_text(size=0, face="bold"), axis.text.x =element_text(size=12, face="bold")) +
+    scale_fill_manual(values = getPalette(colourCount)) +
+    theme_gray() #+ ylim(0,150) #+ coord_flip()
+  but_plot
+  ggsave(paste(outputfile, species, "png", sep='.'), plot=but_plot, device = "png", dpi = 300)
+}
+#
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GroupBox
-Group box plot
+# Language: R
+# Input: TXT
+# Output: PREFIX 
+# Tested with: PluMA 2.0, R 4.0.0
+# Dependencies: ggsci_2.9,hrbrthemes_0.8.0,viridis_0.5.1,viridisLite_0.3.0,gplots_3.1.1,ggpubr_0.4.0,ggplot2_3.3.5
+
+PluMA plugin to produce boxplots of a group of taxa abundances.
+
+The plugin expects as input a TXT file of keyword-value pairs:
+abundance: Taxa abundances, with column 1 the sample and column 2 the group
+taxa: Line-by-line TXT file of taxa to perform plots
+
+Output will be sent to PNG files, one per taxon, using the user-specified prefix and the taxa name.
